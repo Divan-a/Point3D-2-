7786b6eef4f76dacab0a45f117bbd76f7babf9f9
Divan <150805dimaon@gmail.com>
2024-12-03 09:31:40 +0600

    Add Point and Dummy classes

--- src/Main.java
+++ src/Main.java
@@ -89,51 +89,6 @@ public class Main {
     }
 }
 
-class Point3D {
-    private double x;
-    private double y;
-    private double z;
-
-    Point3D() {
-        this.x = 0;
-        this.y = 0;
-        this.z = 0;
-    }
-
-    Point3D(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public double GetX() {
-        return this.x;
-    }
-
-    public double GetY() {
-        return this.y;
-    }
-
-    public double GetZ() {
-        return this.z;
-    }
-
-    public void SetX(double c) {
-        this.x = c;
-    }
-
-    public void SetY(double c) {
-        this.y = c;
-    }
-
-    public void SetZ(double c) {
-        this.z = c;
-    }
-
-    public void displayInfo() {
-        System.out.printf("Координаты точки (%.1f, %.1f, %.1f)\n", GetX(), GetY(), GetZ());
-    }
-}
 
 class Vector3D {
     private double x, y, z;
