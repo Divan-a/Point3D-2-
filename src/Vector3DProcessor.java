131be059e2bf094bbf8e00665f72acd77281962f
Divan <150805dimaon@gmail.com>
2024-12-04 14:55:52 +0600

    cleanup code within Vector3DProcessor

--- src/Vector3DProcessor.java
+++ src/Vector3DProcessor.java
@@ -1,48 +1,33 @@
 public class Vector3DProcessor {
-    public static void Vector_sum(Vector3D vector1, Vector3D vector2) {
-        double newx = vector1.GetX() + vector2.GetX();
-        double newy = vector1.GetY() + vector2.GetY();
-        double newz = vector1.GetZ() + vector2.GetZ();
-        vector1.SetX(newx);
-        vector1.SetY(newy);
-        vector1.SetZ(newz);
-        vector1.Vector_len();
+    public static Vector3D Vector_sum(Vector3D vector1, Vector3D vector2) {
+        Vector3D v3 = new Vector3D();
+        v3.SetX(vector1.GetX() + vector2.GetX());
+        v3.SetY(vector1.GetY() + vector2.GetY());
+        v3.SetZ(vector1.GetZ() + vector2.GetZ());
+        return v3;
     }
 
-    public static void Vector_dif(Vector3D vector1, Vector3D vector2) {
-        double newx = vector1.GetX() - vector2.GetX();
-        double newy = vector1.GetY() - vector2.GetY();
-        double newz = vector1.GetZ() - vector2.GetZ();
-        vector1.SetX(newx);
-        vector1.SetY(newy);
-        vector1.SetZ(newz);
-        vector1.Vector_len();
+    public static Vector3D Vector_dif(Vector3D vector1, Vector3D vector2) {
+        return new Vector3D(
+                vector1.GetX() - vector2.GetX(),
+                vector1.GetY() - vector2.GetY(),
+                vector1.GetZ() - vector2.GetZ()
+        );
     }
 
     public static double Vector_dot_product(Vector3D vector1, Vector3D vector2) {
         return vector1.GetX() * vector2.GetX() + vector1.GetY() * vector2.GetY() + vector1.GetZ() * vector2.GetZ();
     }
 
-    public static void Vector_product(Vector3D vector1, Vector3D vector2) {
-        double newx = vector1.GetY() * vector2.GetZ() - vector2.GetY() * vector1.GetZ();
-        double newy = vector1.GetX() * vector2.GetZ() - vector2.GetX() * vector1.GetZ();
-        double newz = vector1.GetY() * vector2.GetX() - vector2.GetY() * vector1.GetX();
-        vector1.SetX(newx);
-        vector1.SetY(newy);
-        vector1.SetZ(newz);
-        vector1.Vector_len();
+    public static Vector3D Vector_product(Vector3D vector1, Vector3D vector2) {
+        return new Vector3D(
+                vector1.GetY() * vector2.GetZ() - vector2.GetY() * vector1.GetZ(),
+                -1 * (vector1.GetX() * vector2.GetZ() - vector2.GetX() * vector1.GetZ()),
+                vector1.GetY() * vector2.GetX() - vector2.GetY() * vector1.GetX());
     }
 
     public static boolean Vector_collinearity_check(Vector3D vector1, Vector3D vector2) { //вопросы имеются
-        boolean bul = false;
-        double x = vector1.GetX(), y = vector1.GetY(), z = vector1.GetZ();
-        Vector_product(vector1, vector2);
-        if (vector1.GetX() == 0 && vector1.GetY() == 0 && vector1.GetX() == 0) {bul = true;}
-
-        vector1.SetX(x);
-        vector1.SetY(y);
-        vector1.SetZ(z);
-        vector1.Vector_len();
-        return bul;
+        Vector3D vector3D = Vector_product(vector1, vector2);
+        return vector3D.GetX() == 0 && vector3D.GetY() == 0 && vector3D.GetZ() == 0;
     }
 }
